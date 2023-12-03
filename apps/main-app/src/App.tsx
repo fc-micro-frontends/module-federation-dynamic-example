560a8157252dd94dce0d559895fb077f534adc83
Woongjae Lee <2woongjae@gmail.com>
2023-12-03 20:59:47 +0900

    create example

---
+++ apps/main-app/src/App.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+import "./index.css";
+import DynamicButton from "./components/DynamicButton";
+
+const App = () => {
+  const [button, setButton] = React.useState<{
+    url?: string;
+    scope?: string;
+    module?: string;
+  }>({});
+
+  function setButtonFromComponentApp1() {
+    setButton({
+      url: "http://localhost:3001",
+      scope: "component_app1",
+      module: "./Button",
+    });
+  }
+
+  function setButtonFromComponentApp2() {
+    setButton({
+      url: "http://localhost:3002",
+      scope: "component_app2",
+      module: "./Button",
+    });
+  }
+
+  return (
+    <div className="container">
+      <div>Name: main-app</div>
+      <div>Framework: react</div>
+      <div>Language: TypeScript</div>
+      <div>CSS: Empty CSS</div>
+      <button onClick={setButtonFromComponentApp1}>
+        Load Component App 1 Button
+      </button>
+      <button onClick={setButtonFromComponentApp2}>
+        Load Component App 2 Button
+      </button>
+      <div>
+        <DynamicButton button={button} />
+      </div>
+    </div>
+  );
+};
+ReactDOM.createRoot(document.getElementById("app")!).render(<App />);
---
+++ apps/main-app/src/components/DynamicButton.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { importRemote } from "@module-federation/utilities";
+
+interface DynamicButtonProps {
+  button: {
+    url?: string;
+    scope?: string;
+    module?: string;
+  };
+}
+
+const DynamicButton: React.FC<DynamicButtonProps> = ({
+  button: { url, scope, module },
+}) => {
+  if (!url || !scope || !module) {
+    return null;
+  }
+
+  const Component = React.lazy(() => importRemote({ url, scope, module }));
+
+  return (
+    <React.Suspense fallback="Loading System">
+      <Component />
+    </React.Suspense>
+  );
+};
+
+export default DynamicButton;
